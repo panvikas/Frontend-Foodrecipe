afb9b5389d85ff42247b3ba3642096c1881110e2
Vikas <vp999040@gmail.com>
2025-07-25 01:16:25 -0700

    added 1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import RecipeDetails from './pages/RecipeDetails'
 
 const getAllRecipes=async()=>{
   let allRecipes=[]
-  await axios.get('https://backend-foodrecipe-toj3.onrender/recipe').then(res=>{
+  await axios.get('https://backend-foodrecipe-toj3.onrender.com/recipe').then(res=>{
     allRecipes=res.data
   })
   return allRecipes
